113f94c9c7a98b4ffb14e77df7370df620c48ad3
lakotajames <lakota.james@gmail.com>
2015-03-31 07:03:32 -0400

    Button joins lobby
    Next part's borked.

---
+++ Assets/ConnectButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectButton : MonoBehaviour {
+
+	public GameObject connectButton;
+	public GameObject roomList;
+	public GUIText roomText;
+	// Use this for initialization
+	void Start () {
+
+	}
+	public void ConnectClick(){
+		Debug.Log ("CLICKED");
+		PhotonNetwork.ConnectUsingSettings ("ALPHA 0.0.2");
+	}
+
+	void OnGUI(){
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString());
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnJoinedLobby(){
+		Debug.Log ("LOBBY");
+		connectButton.GetComponent<Canvas>().enabled = false;
+		roomList.GetComponent<Canvas> ().enabled = true;
+		roomText.text = PhotonNetwork.GetRoomList ().ToString();
+
+	}
+
+
+}
