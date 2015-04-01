7f905f5bf47dfd2d14849ab95f62cc81e5d3ab22
lakotajames <lakota.james@gmail.com>
2015-03-31 23:03:39 -0400

    Lobby Loads level.
    Cant actually stay in the lobby though.

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -13,47 +13,25 @@ public class NetworkManager : MonoBehaviour {
 	void Start () {
 		gm = GetComponent <GameManager>();
 		pv = GetComponent<PhotonView> ();
-		if (offlinemode) {
-			PhotonNetwork.offlineMode = true;
-		}
-
-		Connect ();
-	}
-
-	void Connect(){
-		if (offlinemode){
-			OnJoinedLobby();
-		}else{
-		PhotonNetwork.ConnectUsingSettings ("ALPHA 0.0.1");
-		}
-	}
-
-	void OnGUI(){
-		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString());
-
-	}
-
-	void OnJoinedLobby(){
-		PhotonNetwork.JoinRandomRoom ();
-	}
-
-	void OnPhotonRandomJoinFailed(){
-		PhotonNetwork.CreateRoom (null);
-	}
-
-	void OnJoinedRoom(){
 		if (PhotonNetwork.isMasterClient) {
 			seed = Random.seed;
 			pv.RPC("BeginGame",PhotonTargets.AllBufferedViaServer,seed);
 		}
 	}
+
+
+
+	void OnGUI(){
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString());
+
+	}
+	
 	public void SpawnMyPlayer(){
 		PlayerSpawn mySpawn = gm.GetSpawn();
 		GameObject MyPlayerGO = PhotonNetwork.Instantiate ("PlayerController", mySpawn.transform.position, mySpawn.transform.rotation, 0);
 		standbyCamera.SetActive (false);
 		(MyPlayerGO.GetComponent ("PlayerMovement") as MonoBehaviour).enabled = true;
 		MyPlayerGO.GetComponent<CharacterController>().enabled = true;
-		//(MyPlayerGO.GetComponent ("SimpleMouseRotator") as MonoBehaviour).enabled = true;
 		((MonoBehaviour)MyPlayerGO.GetComponent("PlayerShoot")).enabled = true;
 		MyPlayerGO.transform.FindChild("FirstPersonCharacter").gameObject.SetActive(true);
 		MyPlayerGO.GetComponentInChildren<Camera> ().enabled = true;
