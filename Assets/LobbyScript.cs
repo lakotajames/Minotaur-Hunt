4c477aa40974f3f065bafeea9bf7eecc06e12720
lakotajames <lakota.james@gmail.com>
2015-04-01 03:33:03 -0400

    Minimum Players works

--- Assets/LobbyScript.cs
+++ Assets/LobbyScript.cs
@@ -1,19 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LobbyScript : MonoBehaviour {
+using UnityEngine.UI;
 
+public class LobbyScript : MonoBehaviour {
+	public Canvas MasterLobby;
+	public InputField MinPlayerBox;
+	private bool ready;
+	private int minPlayers;
 	// Use this for initialization
 	void Start () {
 		PhotonNetwork.automaticallySyncScene = true;
-		if (PhotonNetwork.isMasterClient) {
-
-			PhotonNetwork.LoadLevel ("scene");
+		bool ready = false;
+		if (PhotonNetwork.isMasterClient == false) {
+			MasterLobby.enabled = false;
 		}
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (ready) {
+			if (MasterLobby.isActiveAndEnabled){
+				MasterLobby.enabled = false;
+			}
+			if (PhotonNetwork.isMasterClient){
+				if (PhotonNetwork.playerList.Length >= minPlayers){
+					StartGame();
+				}
+			}
+		}else{
+			if (PhotonNetwork.isMasterClient && !MasterLobby.enabled == false) {
+				Debug.Log(MasterLobby.enabled);
+				MasterLobby.enabled = true;
+			}
+		}
+	}
+	public void StartButton(){
+		minPlayers = int.Parse(MinPlayerBox.text);
+		ready = true;
+	}
+	void StartGame(){
+		PhotonNetwork.LoadLevel ("scene");
 	}
 }
